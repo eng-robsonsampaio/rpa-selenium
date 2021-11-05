e2f7e8d1a54ccce1b78376d0a81f2f194efea735
eng-robsonsampaio <robsonsampaio90@gmail.com>
2021-11-05 17:47:20 -0300

    fix code

--- sitac-crea-bot.py
+++ sitac-crea-bot.py
@@ -17,25 +17,14 @@ element_senha.send_keys(login.PSK)
 
 browser.find_element_by_xpath("//button[@name='entrar']").click()
 
-sleep(5)
-browser.find_element_by_id('mostrarPendencia').click()
-sleep(3)
+# sleep(5)
+# browser.find_element_by_id('mostrarPendencia').click()
+# sleep(3)
 
-# browser.find_element_by_xpath('//*[@id="ToolTables_2b81d69e1cd846edcf6f0b482bb82cf8_0"]').click()
-# browser.find_element_by_id('ToolTables_adce851614cc2cec446efc69bba3a044_0').click()
-browser.find_element_by_xpath('//a[@class="DTTT_button ui-button ui-state-default DTTT_button_text"]').click()
-sleep(3)
+# browser.find_element_by_xpath('//a[@class="DTTT_button ui-button ui-state-default DTTT_button_text"]').click()
+# sleep(3)
 
-browser.find_element_by_id('botao_imprimir_map').click()
-sleep(3)
-
-# browser.find_element_by_id(id_='empresa').click()
-# element.click()
-
-# browser.find_element_by_xpath("//input[@value='empresa']").click()
-# element_empresa = browser.find_element_by_id(id_='empresa').click()
-# sleep(2)
-# element_empresa.click()
-# cpf_input.send_keys('02781607355')
+# browser.find_element_by_id('botao_imprimir_map').click()
+# sleep(3)
 
 input('')
