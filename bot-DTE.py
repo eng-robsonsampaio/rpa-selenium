0d002418421b28f653c7a532e561b2a39cb2898a
robson-sampaio <robson.sampaio@dellead.com>
2021-11-10 20:45:08 -0300

    I entered the siget portal

--- bot-DTE.py
+++ bot-DTE.py
@@ -23,10 +23,29 @@ try:
 finally:
     sleep(1)
     driver.find_element_by_xpath("//a[@routerlink='/certificado']").click()
-    sleep(3)
+    sleep(2)
     driver.find_element_by_xpath("/html/body/my-app/div/div/div/app-certificado/div/ul/li[2]/button").click()
-    sleep(5)
+    sleep(2)
     pyautogui.hotkey('tab', 'tab', 'enter', interval=0.1)
-    
 
+# try:
+#     elem = WebDriverWait(driver, 30).until(
+#         EC.presence_of_element_located(By.XPATH , "//button[@type='button']"))
+# finally:
+    sleep(3)
+    driver.find_element_by_xpath("/html/body/my-app/div/div/div/app-e-cnpj/div/div[2]/table/tbody/tr[1]").click()
+    driver.find_element_by_xpath("/html/body/my-app/div/div/div/app-e-cnpj/div/div[3]/button[2]").click()
+    sleep(2)
+    pyautogui.hotkey('tab', 'tab', 'enter', interval=0.1)
+    sleep(2)
+    driver.find_element_by_xpath("/html/body/my-app/div/div/div/app-perfil/div/div[1]/table/tbody/tr/td[1]").click()
+    driver.find_element_by_xpath("/html/body/my-app/div/div/div/app-perfil/div/div[2]/button[2]").click()
+    sleep(2)
+    driver.find_element_by_xpath("/html/body/my-app/div/div/div/app-home/section/div/div[2]/div/ul/li/a/div").click()
+    sleep(5)
+    pyautogui.hotkey('enter')
+    sleep(3)
+    pyautogui.hotkey('enter')
+
+    # /html/body/my-app/div/div/div/app-home/section/div/div[2]/div/ul/li/a/div
 # input('')
