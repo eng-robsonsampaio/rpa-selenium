877f8231c2c47dee0dbbc2f5d9af309c6af38951
robson-sampaio <robson.sampaio@dellead.com>
2021-11-10 20:18:44 -0300

    Using pyautogui to click on windows warning

--- bot-DTE.py
+++ bot-DTE.py
@@ -8,20 +8,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+import pyautogui
+
+
+
 options = webdriver.ChromeOptions()
 options.add_extension(r'.\extensions\Web-PKI.crx')
 
-browser = webdriver.Chrome(r'.\drivers\chromedriver.exe', options=options)
-browser.get(login_dte.URL)
+driver = webdriver.Chrome(r'.\drivers\chromedriver.exe', options=options)
+driver.get(login_dte.URL)
 try:
-    elem = WebDriverWait(browser, 30).until(
+    elem = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located(By.XPATH , "//a[@routerlink='/certificado']"))
 finally:
     sleep(1)
-    browser.find_element_by_xpath("//a[@routerlink='/certificado']").click()
+    driver.find_element_by_xpath("//a[@routerlink='/certificado']").click()
     sleep(3)
-    browser.find_element_by_xpath("/html/body/my-app/div/div/div/app-certificado/div/ul/li[2]/button").click()
-    sleep(3)
-    alert = browser.switch_to_alert()
+    driver.find_element_by_xpath("/html/body/my-app/div/div/div/app-certificado/div/ul/li[2]/button").click()
+    sleep(5)
+    pyautogui.hotkey('tab', 'tab', 'enter', interval=0.1)
+    
 
-input('')
+# input('')
