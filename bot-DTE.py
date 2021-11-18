8953861f21725d7d45b6b57a8d465a695a02e652
robson-sampaio <robson.sampaio@dellead.com>
2021-11-18 08:12:30 -0300

    click_on_element criado

--- bot-DTE.py
+++ bot-DTE.py
@@ -32,40 +32,54 @@ p = driver.current_window_handle
 print(f'Windows title:  {driver.title}')
 print(f'Windows habdke:  {p}')
 
-sleep(2)
-elem = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//a[@routerlink='/certificado']")))
-elem.click()
 
-elem = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-certificado/div/ul/li[2]/button")))
-elem.click()
-sleep(3)
-pyautogui.hotkey('tab', 'tab', 'enter', interval=0.1)
-
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-e-cnpj/div/div[2]/table/tbody/tr[1]"))
-    )
-elem.click()    
-
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-e-cnpj/div/div[3]/button[2]"))
-    )
-elem.click()
-sleep(3)
-pyautogui.hotkey('tab', 'tab', 'enter', interval=0.1)
-
-elem = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-perfil/div/div[1]/table/tbody/tr/td[1]")))
-elem.click()
-
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-perfil/div/div[2]/button[2]"))
-    )
-elem.click()
-
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-home/section/div/div[2]/div/ul/li/a/div"))
-    )
+def click_on_element(method, path):
+    try:
+        elem = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((method, path)))
+        elem.click()
+    except:
+        print(f'Can not click on {path}')
     
-elem.click()
+
+sleep(2)
+click_on_element(By.XPATH, "//a[@routerlink='/certificado']")
+# elem = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//a[@routerlink='/certificado']")))
+# elem.click()
+click_on_element(By.XPATH, "/html/body/my-app/div/div/div/app-certificado/div/ul/li[2]/button")
+# elem = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-certificado/div/ul/li[2]/button")))
+# elem.click()
+sleep(3)
+pyautogui.hotkey('tab', 'tab', 'enter', interval=0.1)
+click_on_element(By.XPATH, "/html/body/my-app/div/div/div/app-e-cnpj/div/div[2]/table/tbody/tr[1]")
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-e-cnpj/div/div[2]/table/tbody/tr[1]"))
+#     )
+# elem.click()    
+click_on_element(By.XPATH, "/html/body/my-app/div/div/div/app-e-cnpj/div/div[3]/button[2]")
+
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-e-cnpj/div/div[3]/button[2]"))
+#     )
+# elem.click()
+sleep(3)
+pyautogui.hotkey('tab', 'tab', 'enter', interval=0.1)
+
+click_on_element(By.XPATH, "/html/body/my-app/div/div/div/app-perfil/div/div[1]/table/tbody/tr/td[1]")
+# elem = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-perfil/div/div[1]/table/tbody/tr/td[1]")))
+# elem.click()
+
+click_on_element(By.XPATH, "/html/body/my-app/div/div/div/app-perfil/div/div[2]/button[2]")
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-perfil/div/div[2]/button[2]"))
+#     )
+# elem.click()
+
+click_on_element(By.XPATH, "/html/body/my-app/div/div/div/app-home/section/div/div[2]/div/ul/li/a/div")
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.XPATH, "/html/body/my-app/div/div/div/app-home/section/div/div[2]/div/ul/li/a/div"))
+#     )
+    
+# elem.click()
 sleep(1)
 print(f'Número de janelas: {len(driver.window_handles)}')
 driver.switch_to.window(driver.window_handles[-1])
@@ -85,27 +99,31 @@ print(f'Windows habdke:  {p}')
 siget_url = driver.current_url
 print(f'\n\nURL{siget_url}\n\n')
 
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.ID, 'menu_indicadores_nfe'))
-    )   
+click_on_element(By.ID, 'menu_indicadores_nfe')
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.ID, 'menu_indicadores_nfe'))
+#     )   
 print('Menu indicadores NFe')
-elem.click()
+# elem.click()
 
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[1]/button'))
-    )
+click_on_element(By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[1]/button')
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[1]/button'))
+#     )
 print('Pesquisar')
-elem.click()
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[2]/div/button'))
-    )
+# elem.click()
+click_on_element(By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[2]/div/button')
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[2]/div/button'))
+#     )
 print('Download')
-elem.click()    
+# elem.click()    
+click_on_element(By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[2]/div/ul/li[2]/a')
 
-elem = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[2]/div/ul/li[2]/a'))
-    )
+# elem = WebDriverWait(driver, 30).until(
+#         EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/app-nfe/div/div/section[2]/div/div/div/div/app-nfe-entradas/div[1]/div[2]/div[2]/div/ul/li[2]/a'))
+#     )
 print('Download CSV')
-elem.click()
+# elem.click()
   
 print(f'---- {(time() - start_time)} seconds ----')
